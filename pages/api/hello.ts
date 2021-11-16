2b28603e637a3af7ea66470401c9733250141c1d
Zach Pinfold <zach.pinfold@digitalworkroom.co.uk>
2021-11-16 12:16:40 +0100

    test colormind fetch in the API

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const url = "http://colormind.io/api/";
+  const data = {
+    model: "default",
+  };
+  const headers = {
+    "Content-Type": "text/plain",
+  };
+  axios
+    .post(url, data, { headers })
+    .then((d) => res.status(200).json({ name: d.data.result }));
 }
