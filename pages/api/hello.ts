db71b5e7bd315c455f0464feb010288ddfa6acee
Zach Pinfold <zach.pinfold@digitalworkroom.co.uk>
2021-11-18 10:09:31 +0100

    added catch to api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,20 +3,31 @@ import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string;
+  colourData: number[] | string;
 };
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const url = "http://colormind.io/api/";
-  const data = {
-    model: "default",
-  };
-  const headers = {
-    "Content-Type": "text/plain",
-  };
-  const result = await axios.post(url, data, { headers });
-  const responseData = result.data.result;
+  try {
+    const url = "http://colormind.io/api/";
+    const data = {
+      model: "default",
+    };
+    const headers = {
+      "Content-Type": "text/plain",
+    };
+    const result = await axios.post(url, data, { headers });
+    const responseData = result.data.result;
 
-  res.status(200).json({ name: responseData });
+    res.status(200).json({ colourData: responseData });
+  } catch (error) {
+    if (error) {
+      let errorMessage = "Failed to return colour";
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+      console.log(errorMessage);
+      return res.status(500).json({ colourData: errorMessage });
+    }
+  }
 };
 
 export default handler;
