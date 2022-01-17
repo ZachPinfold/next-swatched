778b0a094f06ae5d41ef6db7a2813ae6a5e19706
Zach Pinfold <zach.pinfold@digitalworkroom.co.uk>
2022-01-17 11:24:46 +0000

    added tests to dev

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   testEnvironment: "jsdom",
+  transform: {
+    // "^.+\\.tsx?$": "<rootDir>/node_modules/jest",
+    "^.+\\.svg$": "<rootDir>/svgTransform.js",
+  },
 };
