685a11dad32c1a45e2d2534727fac02e9d923c4a
Zach Pinfold <zach.pinfold@digitalworkroom.co.uk>
2022-01-17 13:58:07 +0000

    need to add redux to tests

--- __tests__/SwatchCard.test.js
+++ __tests__/SwatchCard.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import Footer from "../components/footer/Footer.tsx";
+import SwatchPicker from "../components/swatch picker/SwatchPicker";
 
 test("Check for Getting Started Text", () => {
-  const { getByText } = render(<Footer />);
+  const { getByText } = render(<SwatchPicker />);
   expect(getByText("Privacy")).toBeInTheDocument();
 });
