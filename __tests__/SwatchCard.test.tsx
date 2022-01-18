60dbb537fc858c31a4abd43ec67a54426d3be485
Zach Pinfold <zach.pinfold@digitalworkroom.co.uk>
2022-01-18 09:06:34 +0000

    add enzyme testing to Jest

--- __tests__/SwatchCard.test.tsx
+++ __tests__/SwatchCard.test.tsx
@@ -1,11 +1,10 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import HomepageSwatchCard from "../components/swatch picker/swatch card/HomepageSwatchCard";
-import { Provider } from "react-redux";
+import { shallow, mount } from "enzyme";
+import React from "react";
 import CopyImage from "../../assets/images/copy_swatch.svg";
-
+import HomepageSwatchCard from "../components/swatch picker/swatch card/HomepageSwatchCard";
 import createMockStore from "redux-mock-store";
 import { CardHover } from "../types/swatches";
+import { Provider } from "react-redux";
 
 const swatch = [2, 3, 4];
 
@@ -24,6 +23,20 @@ const cardHover: CardHover[] = [
     type: "copy",
     image: CopyImage,
     message: "copy hex",
+    clickedMessage: "hex copied!",
+    func: funcMock,
+  },
+  {
+    type: "copy",
+    image: CopyImage,
+    message: "save to swatch",
+    clickedMessage: "hex copied!",
+    func: funcMock,
+  },
+  {
+    type: "copy",
+    image: CopyImage,
+    message: "lock color",
     clickedMessage: "hex copied!",
     func: funcMock,
   },
@@ -46,9 +59,21 @@ const TestComponent = (
   </Provider>
 );
 
-test("Check for Getting Started Text", () => {
-  const cards: any = render(TestComponent).container.firstChild;
-  const { className } = cards;
-  expect(className).toEqual("colour_card");
-  expect(cards).toHaveLength(2);
+describe("With Enzyme", () => {
+  it("The homepage renders out the appropriate text within the swatch cards", () => {
+    const app = shallow(TestComponent).dive().dive();
+
+    expect(app.find("h5").first().text()).toEqual(cardHover[0].message);
+    expect(app.find("h5").at(1).text()).toEqual(cardHover[1].message);
+    expect(app.find("h5").at(2).text()).toEqual(cardHover[2].message);
+  });
+  it("The homepage card renders out with approrpriate background colour", () => {
+    const app = shallow(TestComponent).dive().dive();
+
+    let containerStyle = app.find(".colour_card").get(0).props.style;
+
+    expect(
+      app.find(".colour_card").get(0).props.style.backgroundColor
+    ).toContain("rgb(2,3,4)");
+  });
 });
